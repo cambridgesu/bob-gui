2e10324292cdeaa6dcfdfa35f1814da2029c6c22
mvl22 <martin@lucas-smith.co.uk>
2018-12-02 21:17:03 +0000

    Add number formatting to voter list clone

--- controlpanel/bobguiAdminister.php
+++ controlpanel/bobguiAdminister.php
@@ -1545,7 +1545,7 @@ class bobguiAdminister extends frontControllerApplication
 			</ul>
 		");
 		if ($voterListCloned) {
-			$form->heading ('', "<p class=\"warning\">A suggested list of " . count ($currentVoters) . " voters has been pre-filled, as a starting point, copied from the <a target=\"_blank\" title=\"[Link opens in a new tab/window]\" href=\"{$this->settings['liveServerUrl']}{$voterListCloned['url']}\">last ballot ('" . htmlspecialchars ($voterListCloned['title']) . "')</a> that was set up (completely) for this organisation in the current academic year.</p>\n<p class=\"warning\"><strong>The list below has not yet been saved.</strong>");
+			$form->heading ('', "<p class=\"warning\">A suggested list of " . number_format (count ($currentVoters)) . " voters has been pre-filled, as a starting point, copied from the <a target=\"_blank\" title=\"[Link opens in a new tab/window]\" href=\"{$this->settings['liveServerUrl']}{$voterListCloned['url']}\">last ballot ('" . htmlspecialchars ($voterListCloned['title']) . "')</a> that was set up (completely) for this organisation in the current academic year.</p>\n<p class=\"warning\"><strong>The list below has not yet been saved.</strong>");
 		}
 		$form->textarea (array (
 			'name'					=> 'voters',
